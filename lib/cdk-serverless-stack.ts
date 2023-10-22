a271107ce1e7b64afb9c3858c325dcf14a19107c
koh124 <altkohei212@gmail.com>
2023-10-22 21:44:15 +0900

    サーバレス構成を作成

--- lib/cdk-serverless-stack.ts
+++ lib/cdk-serverless-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,9 +10,21 @@ export class CdkServerlessStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambdaを作成する
+    const lambdaFunc = new lambda.Function(this, 'CdkServerlessLambda', {
+      runtime: lambda.Runtime.NODEJS_16_X, // ランタイムにはNode.js 16.xを指定
+      code: lambda.Code.fromAsset('lambda'), // lambdaディレクトリにあるコードを指定
+      handler: 'index.handler', // ハンドラを指定する。index.jsのhandlerを指定。
+    });
+
+    // API Gatewayを作成する
+    const apigw = new apigateway.RestApi(this, 'CdkServerlessApi', {
+      restApiName: 'CdkServerlessApi', // API名を指定
+      defaultIntegration: new apigateway.LambdaIntegration(lambdaFunc) // Lambda統合を指定する
+    });
+
+    // リソースを追加する
+    const helloResource = apigw.root.addResource('hello'); // /hello リソースが作成される
+    helloResource.addMethod('GET'); // this maps to the GET method of the Lambda function by default
   }
 }
